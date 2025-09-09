337c3977e3c47459cae871ad6fd16746bde77de0
hex01e <houmanso@student.1337.ma>
2025-09-09 02:25:13 +0100

    chore: update tab settings for consistent indentation

--- init.lua
+++ init.lua
@@ -8,4 +8,8 @@ require("config.lazy")
 -- [[ Configuration ]]
 -- Disable expanding <Tab>
 vim.cmd("set noexpandtab")
+-- Set tab width to 2 spaces
+vim.cmd("set tabstop=2")
+-- Set indentation width to 2 spaces
+vim.cmd("set shiftwidth=2")
 
