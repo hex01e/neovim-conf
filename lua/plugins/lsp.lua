8e5ecbc3e81a8db22c65bf75c5fea29e488ce3d0
hex01e <houmanso@student.1337.ma>
2025-09-09 23:06:52 +0100

    feat: add LSP configuration for Mason and nvim-lspconfig

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,50 @@
+-- LSP configurations
+return {
+	-- Mason core
+	{
+		"mason-org/mason.nvim",
+		opts = {}
+	},
+
+	-- Mason lsconfig
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {	
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls",
+			}
+		},
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			{ "mason-org/mason.nvim", opts = {} },
+		},
+	},
+
+	-- Nvim lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+
+			-- Lua
+			lspconfig.lua_ls.setup({})
+			-- Js/ts
+			lspconfig.ts_ls.setup({})
+
+			-- keymaps
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+			vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+
+			-- Show diagnostics as virtual text and underlines
+			vim.diagnostic.config({
+				virtual_text = true,   -- show message inline
+				signs = true,          -- keep the "E"/"W" in the gutter
+				underline = true,      -- underline the text with issues
+				update_in_insert = false,
+				severity_sort = true,
+			})
+		end
+	},
+}
