6212df82aaea0818a54a1b2c0eeac5f4622b7d9c
hex01e <houmanso@student.1337.ma>
2025-09-09 02:26:40 +0100

    feat: add  and set Catppuccin colorscheme configuration

---
+++ lua/plugins/cappuccin.lua
@@ -0,0 +1,10 @@
+-- Catppuccin colorschema
+return {
+  "catppuccin/nvim", 
+  name = "catppuccin", 
+  priority = 1000,
+  config = function()
+    -- load the colorscheme here
+    vim.cmd([[colorscheme catppuccin]])
+  end,
+}
