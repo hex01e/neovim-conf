098d234debaf61fbd8d60696f194ba9ce496b7ea
hex01e <houmanso@student.1337.ma>
2025-09-09 03:59:58 +0100

    feat: enable line numbering and highlight current line in configuration

--- init.lua
+++ init.lua
@@ -15,3 +15,9 @@ vim.opt.tabstop = 2
 -- Set indentation width to 2 spaces
 vim.opt.shiftwidth = 2
 
+-- Enable line numbers
+vim.opt.number = true
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+-- Highlight the current line
+vim.opt.cursorline = true
