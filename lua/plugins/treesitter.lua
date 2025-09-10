c027b8fe1bcbbb4e12b51e983dc6de781de2e4c5
hex01e <houmanso@student.1337.ma>
2025-09-10 13:03:17 +0100

    refactor: remove ensure_install and keep auto_install for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup {
-			ensure_installed = { "lua", "python", "javascript", "html", "css", "json", "bash", "typescript", "tsx" },
 			highlight = { enable = true },
 			indent = { enable = true },
 			incremental_selection = { enable = true },
