a3baebe7278923f0d05e06e95126c7fb03a58c8d
hex01e <houmanso@student.1337.ma>
2025-09-13 22:21:13 +0100

    chore: re-organize the structer by isolate options

--- init.lua
+++ init.lua
@@ -2,26 +2,8 @@
 ---  Created by hex01e   ---
 ----------------------------
 
--- [[ Configuration ]]
+-- Neovim configuration options
+require('config.options')
 
--- Disable expanding <Tab>
-vim.opt.expandtab = false
--- Set tab width to 2 spaces
-vim.opt.tabstop = 2
--- Set indentation width to 2 spaces
-vim.opt.shiftwidth = 2
-
--- Enable line numbers
-vim.opt.number = true
--- Enable relative line numbers
-vim.opt.relativenumber = true
--- Highlight the current line
-vim.opt.cursorline = true
-
--- Disable wraping lines
-vim.opt.wrap = false
-
-
--- [[ Lazy.nvim ]]
-
+-- Package manger (Lazy.nvim)
 require("config.lazy")
---
+++ lua/config/options.lua
@@ -0,0 +1,19 @@
+-- [[ Configuration ]]
+
+-- Disable expanding <Tab>
+vim.opt.expandtab = false
+-- Set tab width to 2 spaces
+vim.opt.tabstop = 2
+-- Set indentation width to 2 spaces
+vim.opt.shiftwidth = 2
+
+-- Enable line numbers
+vim.opt.number = true
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+-- Highlight the current line
+vim.opt.cursorline = true
+
+-- Disable wraping lines
+vim.opt.wrap = false
+
